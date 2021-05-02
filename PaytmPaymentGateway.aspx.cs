e694e9472582b718b12b5dfb7d5a964543e4d288
100rabh05 <100rabhjaiswal05@gmail.com>
2021-05-02 12:50:31 +0530

    Add files via upload

---
+++ PaytmPaymentGateway.aspx.cs
@@ -0,0 +1,55 @@
+﻿using paytm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PayTMPaymentGateway
+{
+    public partial class PaytmPaymentGateway : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+			String merchantKey = //"Enter Your Merchant key";
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			parameters.Add("MID", "Merchant ID");
+			parameters.Add("CHANNEL_ID", "WEB");
+			parameters.Add("INDUSTRY_TYPE_ID", "Retail");
+			parameters.Add("WEBSITE", "WEBSTAGING");
+			parameters.Add("EMAIL", "100rabhjaiswal05@gmail.com");
+			parameters.Add("MOBILE_NO","123456789");
+			parameters.Add("CUST_ID", "CUST_001");
+			parameters.Add("ORDER_ID", "001");
+			parameters.Add("TXN_AMOUNT","1");
+			parameters.Add("CALLBACK_URL", "https://localhost:44399/Response.aspx"); //This parameter is not mandatory. Use this to pass the callback url dynamically.
+			string checksum = CheckSum.generateCheckSum(merchantKey, parameters);
+			string paytmURL = "https://securegw-stage.paytm.in/order/process";
+			string outputHTML = "<html>";
+			outputHTML += "<head>";
+			outputHTML += "<title>Merchant Check Out Page</title>";
+			outputHTML += "</head>";
+			outputHTML += "<body>";
+			outputHTML += "<center>Please do not refresh this page...</center>"; //you can put h1 tag here
+			outputHTML += "<form method='post' action='" + paytmURL + "' name='f1'>";
+			outputHTML += "<table border='1'>";
+			outputHTML += "<tbody>";
+			foreach (string key in parameters.Keys)
+			{
+				outputHTML += "<input type='hidden' name='" + key + "' value='" + parameters[key] + "'>";
+			}
+			outputHTML += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "'>";
+			outputHTML += "</tbody>";
+			outputHTML += "</table>";
+			outputHTML += "<script type='text/javascript'>";
+			outputHTML += "document.f1.submit();";
+			outputHTML += "</script>";
+			outputHTML += "</form>";
+			outputHTML += "</body>";
+			outputHTML += "</html>";
+			Response.Write(outputHTML);
+		}
+	}
+    
+}
---
+++ Response.aspx.cs
@@ -0,0 +1,51 @@
+﻿using paytm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PayTMPaymentGateway
+{
+    public partial class Response : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+			String merchantKey = //"Enter Your Merchant Key";
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			string paytmChecksum = "";
+			foreach (string key in Request.Form.Keys)
+			{
+				parameters.Add(key.Trim(), Request.Form[key].Trim());
+			}
+			if (parameters.ContainsKey("CHECKSUMHASH"))
+			{
+				paytmChecksum = parameters["CHECKSUMHASH"];
+				parameters.Remove("CHECKSUMHASH");
+			}
+			if (CheckSum.verifyCheckSum(merchantKey, parameters, paytmChecksum))
+			{
+				string paytmStatus = parameters["STATUS"];
+				string txnId = parameters["TXNID"];
+			
+				if (paytmStatus == "TXN_SUCCESS")
+				{
+				 Label1.Text="Your payment is success";
+				}
+				else if (paytmStatus == "PENDING")
+				{
+					Label1.Text ="Payment is pending !";
+				}
+				else if (paytmStatus == "TXN_FAILURE")
+				{
+					Label1.Text ="Payment Failure !";
+				}
+			}
+			else
+			{
+				Response.Write("Checksum MisMatch");
+			}
+		}
+    }
+}
